6d373c9cf994098926f6590d132da42036b5cab8
anudeepballa <anudeepballa7@gmail.com>
2021-03-25 20:52:00 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MyAuthGaurd } from './myauthgaurd';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+    
   ],
-  providers: [],
+  providers: [MyAuthGaurd],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
